6193d7de4137daaef04da095ad6d4a1211ed1ac9
tinyu <tinyu45O@163.com>
2018-03-07 14:12:41 +0800

    Tanks 项目初始化

---
+++ Assets/scripts/TankMove.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankMove : MonoBehaviour {
+
+	public float mSpeed=5;          //前进速度
+	public float mRotateSpeed=18;   //旋转速度18°
+	public int number=1;            //坦克编号  区分敌方和我方
+
+
+	Rigidbody mRi;
+
+	void Awake()
+	{
+		mRi = GetComponent < Rigidbody> ();
+	}
+
+
+	void FixedUpdate()
+	{
+		float v = Input.GetAxis ("VerticalTank"+number);           //移动方向 w s
+		mRi.velocity = transform.forward * v * mSpeed;  //添加速度   前进
+ 	
+		float h=Input.GetAxis("HorizontalTank"+number);
+		mRi.angularVelocity=transform.up*h*mRotateSpeed;  //旋转
+	}
+}
---
+++ Assets/scripts/UIStart.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIStart : MonoBehaviour {
+
+	public GameObject gameIntroPanel; //游戏说明
+
+	//开始游戏
+	public void StartGame()
+	{
+		UnityEngine.SceneManagement.SceneManager.LoadScene ("Main");	
+	}
+
+	//打开游戏说明界面
+	public void OpenGameIntro()
+	{   
+		if(!gameIntroPanel.activeSelf)
+		gameIntroPanel.SetActive (true);
+	}
+
+
+	//关闭游戏说明界面
+	public void CloseGameIntro()
+	{
+		if(gameIntroPanel.activeSelf)
+		gameIntroPanel.SetActive (false);
+	}
+
+
+	//退出游戏‘
+	public void QuitGame()
+	{
+
+		Application.Quit ();   //退出
+	}
+
+
+
+	void Awake()
+	{
+		gameIntroPanel.SetActive (false);
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
